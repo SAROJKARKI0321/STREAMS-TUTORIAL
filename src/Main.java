f144d7ca83b3bbee30bcd83c1b3aef296fc31247
saroj <sarojkarki0321@gmail.com>
2025-02-14 11:57:16 -0600

    first commit

---
+++ src/Main.java
@@ -0,0 +1,37 @@
+import javax.crypto.spec.PSource;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+import java.util.OptionalDouble;
+import java.util.stream.Collectors;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] arr={64,25,12,22,11,9,54};
+        int n=arr.length;
+
+        for(int i=1;i<n;i++){
+            int key=arr[i];
+            int j=i-1;
+
+            while (j>=0 &&arr[j]>key){
+                arr[j+1]=arr[j];
+                j--;
+
+
+            }
+            arr[j+1]=key;
+            for(int p:arr){
+                System.out.print(p+" ");
+            }
+            System.out.println();
+        }
+        // something is wrong sure...
+
+
+
+
+
+
+    }
+}
